ac34c0709a152ae20e19df1e100c88fce1f656d4
actanonverbos <isaacanthonyx@gmail.com>
2024-08-13 02:31:29 +0800

    Update chain in SwapWrapper.tsx

--- src/components/SwapWrapper.tsx
+++ src/components/SwapWrapper.tsx
@@ -25,7 +25,7 @@ export default function SwapComponents() {
       image: "https://dynamic-assets.coinbase.com/dbb4b4983bde81309ddab83eb598358eb44375b930b94687ebe38bc22e52c3b2125258ffb8477a5ef22e33d6bd72e32a506c391caa13af64c00e46613c3e5806/asset_icons/4113b082d21cc5fab17fc8f2d19fb996165bcce635e6900f7fc2d57c4ef33ae9.png"
   };
  
-  const MochiToken: Token = {
+  const MigglesToken: Token = {
       address: "0xB1a03EdA10342529bBF8EB700a06C60441fEf25d",
       chainId: 8453,
       decimals: 6,
@@ -34,7 +34,7 @@ export default function SwapComponents() {
       image: "https://i.ibb.co.com/CmxSVFc/IMG-0263.png"
   };
  
-  const swappableTokens: Token[] = [ETHToken, MochiToken];
+  const swappableTokens: Token[] = [ETHToken, MigglesToken];
  
   return (
     address ? (
@@ -49,7 +49,7 @@ export default function SwapComponents() {
         <SwapAmountInput className='bg-[#F0F0F0] text-white'
           label="Buy"
           swappableTokens={swappableTokens} 
-          token={MochiToken} 
+          token={MigglesToken} 
           type="to"
         /> 
         <SwapButton className='bg-[#0052FF] text-white'/> 
